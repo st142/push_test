84da0b1cb4dbea0f2e19c3f9e1a5fb089d3b4626
sayaka <sayaka@mail.jp>
2021-08-30 12:52:18 +0900

    change 08301252

--- hello_1/hello3.c
+++ hello_1/hello3.c
@@ -3,10 +3,6 @@
 void main(int argc, char *argv[])
 {
     printf("6th commit\n");
-<<<<<<< HEAD
-	printf("change ƒ[ƒJƒ‹\n");  
-=======
-    printf("change ãƒªãƒ¢ãƒ¼ãƒˆ\n");    
->>>>>>> 297965917f4d15cff1a16d1808a8115580ffcd08
+	printf("change\n");
 }
 
